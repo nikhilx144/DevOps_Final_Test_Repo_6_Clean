32a8fc93d70150f8192b01941082845a2258e06c
Nikhil <nikhilgelli1@gmail.com>
2025-10-23 15:48:38 +0530

    3rd commit

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket         = "nikhil-devops-terraform-state-bucket "
+        bucket         = "nikhil-devops-terraform-state-bucket"
         key            = "global/s3/terraform.tfstate"
         region         = "ap-south-2"
         encrypt        = true
