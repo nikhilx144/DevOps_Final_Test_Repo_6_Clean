664fc96ec58ddc2bb5d28927ae09abf5dc420c6d
Nikhil <nikhilgelli1@gmail.com>
2025-10-24 10:37:04 +0530

    2nd commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_iam_role" "ec2_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole"
+      Action    = "sts:AssumeRole" 
       Effect    = "Allow"
       Principal = {
         Service = "ec2.amazonaws.com"
